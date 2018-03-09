360b655aa67f800bab93471092982d169acddeb9
Mark <jardenilmark@gmail.com>
2018-03-09 13:56:08 +0800

    chore: add components

--- frontend.jsx
+++ frontend.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './src/components/Header.jsx'
+import SearchBar from './src/components/SearchBar.jsx'
 
 const mount = document.getElementById('mount')
 
 ReactDOM.render(
   <div>
     <Header />
+    <SearchBar />
   </div>,
   mount
 )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, FormControl, Button } from 'react-bootstrap'
 
 class Header extends React.Component {
   render () {
     return (
-      <Navbar>
+      <Navbar fluid inverse staticTop>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#home">CRDLR</a>
+            CRDLR
           </Navbar.Brand>
+          <Navbar.Toggle />
         </Navbar.Header>
-        <Nav>
-        </Nav>
+        <Navbar.Collapse>
+          <Navbar.Form pullRight>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search" />
+            </FormGroup>{' '}
+            <Button type="submit">Submit</Button>
+          </Navbar.Form>
+        </Navbar.Collapse>
       </Navbar>
     )
   }
