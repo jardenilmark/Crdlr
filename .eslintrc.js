e71815de8ee085e7364587879ee5994fc9a79d12
Mark <jardenilmark@gmail.com>
2018-03-23 19:49:58 +0800

    docs: add some configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "standard"
+    "extends": [
+      "standard",
+      "plugin:react/recommended"
+    ],
+    "env": { 
+      jest: true,
+      node: true,
+      browser: true,
+      es6: true
+    },
+    "plugins": [
+      "react",
+      "jsx"
+    ]
 };
