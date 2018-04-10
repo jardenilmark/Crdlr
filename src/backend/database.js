b5730b9741ba09dd492963504c1bedea9052f361
Mark <jardenilmark@gmail.com>
2018-04-10 12:44:37 +0800

    style: change to single quotes

--- src/backend/database.js
+++ src/backend/database.js
@@ -2,12 +2,12 @@ const firebase = require('firebase')
 require('firebase/firestore')
 
 const config = {
-  apiKey: "AIzaSyB-5aJfBWUh86Q92gDnanBIoRCpsC7Hlic",
-  authDomain: "crdlr-abfe7.firebaseapp.com",
-  databaseURL: "https://crdlr-abfe7.firebaseio.com",
-  projectId: "crdlr-abfe7",
-  storageBucket: "crdlr-abfe7.appspot.com",
-  messagingSenderId: "691462554775"
+  apiKey: 'AIzaSyB-5aJfBWUh86Q92gDnanBIoRCpsC7Hlic',
+  authDomain: 'crdlr-abfe7.firebaseapp.com',
+  databaseURL: 'https://crdlr-abfe7.firebaseio.com',
+  projectId: 'crdlr-abfe7',
+  storageBucket: 'crdlr-abfe7.appspot.com',
+  messagingSenderId: '691462554775'
 }
 firebase.initializeApp(config)
 const firestore = firebase.firestore()
