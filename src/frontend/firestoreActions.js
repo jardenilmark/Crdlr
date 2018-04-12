9fd806868caa9c0ad76f3ad9842e797047b7b00f
Mark <jardenilmark@gmail.com>
2018-04-12 14:34:31 +0800

    refactor: remove unused async from firestoreActions

--- src/frontend/firestoreActions.js
+++ src/frontend/firestoreActions.js
@@ -1,28 +1,28 @@
 import { firestore } from '../backend/database'
 
-export async function addToDb (collection, object) {
+export function addToDb (collection, object) {
   return firestore.collection(collection).add(object)
 }
 
-export async function getDocument (collection, field, data) {
+export function getDocument (collection, field, data) {
   if (field && data !== undefined) { // data might be a true or false
     return firestore.collection(collection).where(field, '==', data).get()
   }
   return firestore.collection(collection).get()
 }
 
-export async function getDocumentUID (collection, uid) {
+export function getDocumentUID (collection, uid) {
   return firestore.collection(collection).doc(uid).get()
 }
 
-export async function setDocument (collection, id, obj, callback) {
+export function setDocument (collection, id, obj, callback) {
   return firestore.collection(collection).doc(id).set(obj)
 }
 
-export async function updateDocument (collection, id, obj) {
+export function updateDocument (collection, id, obj) {
   return firestore.collection(collection).doc(id).update(obj)
 }
 
-export async function deleteDocument (collection, id) {
+export function deleteDocument (collection, id) {
   return firestore.collection(collection).doc(id).delete()
 }
